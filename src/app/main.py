b12e65eefc4e570ec66db260978bfe06ff56da09
Julien <julien.chaplet@gmail.com>
2023-05-19 12:36:16 +0200

    fixed app

--- src/app/main.py
+++ src/app/main.py
@@ -29,11 +29,11 @@ def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Basic"},
         )
-    return credentials.username
+    return users_db[users_db["username"]==username]["right"].iloc[0]
 
 def get_admin(credentials: HTTPBasicCredentials = Depends(security)):
     username = credentials.username
-    if (username != "admin"):
+    if users_db[(users_db["username"]==username) & (users_db["right"]=="admin")].any().sum() == 0:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="You are not admin",
@@ -45,12 +45,12 @@ def get_admin(credentials: HTTPBasicCredentials = Depends(security)):
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Basic"},
         )
-    return credentials.username
+    return users_db[users_db["username"]==username]["right"].iloc[0]
 
 # welcome
 @api.get('/', name='Check if working')
-def greetings(username: str = Depends(get_current_user)):
-   return {"Welcome": f"Hello {username}"}
+def greetings(right: str = Depends(get_current_user)):
+   return {"Welcome": f"Hello, you have {right} rights"}
 
 
 # L'admin peut voir la liste des utilisateurs et leurs droits
@@ -59,35 +59,51 @@ class User(BaseModel):
     password: str
     right: str
 
-@api.get('/admin', name='')
-def get_users(user: User, username: str = Depends(get_admin)):
-   return {"Users": "List of users and their rights"}   
+@api.get('/admin', name='Get all the users and their right')
+def get_users(username: str = Depends(get_admin)):
+    list_of_user_dicts = users_db.to_dict(orient='records')
+    for user in list_of_user_dicts:
+        user.pop("password")
+    return list_of_user_dicts
 
 
-# L'admin peut rajouter des utilisateurs dans la BDD" (A voir pour la gestion du mot de passe)
-@api.put('/admin', name='')
-def put_users(user: User, username: str = Depends(get_admin)):
+# L'admin peut rajouter des utilisateurs dans la BDD (A voir pour la gestion du mot de passe)
+@api.put('/admin', name='Add user to the database')
+def put_users(user: User, right: str = Depends(get_admin)):
    # Dictionary that we want to add to csv database
-   users_dict = users_db.do_dict(orient='records')
+   new_user_dict = user.dict()
    # list of column names
-   field_names = list(users_dict.keys())
+   field_names = list(new_user_dict.keys())
    # Opening CSV file in append mode and creating a file object for this file
    with open('users_db.csv', 'a') as f_object:
       # we pass the file object and the list of column names to DictWriter()
       dictwriter_object = DictWriter(f_object, fieldnames=field_names)
       # we pass the dictionary as an argument to the Writerow()
-      dictwriter_object.writerow(users_dict)
+      dictwriter_object.writerow(new_user_dict)
       # we close the file object
       f_object.close()
 
-   return #users added 
+   return {"Status": "User added to the database"}
 
 
 # L'admin peut rajouter des droits aux utilisateurs dans la BDD (A voir pour la gestion du mot de passe)
-@api.post('/admin', name='')
-def post_users(username: str = Depends(get_admin)):
-   return #user right added   
+class UserRight(BaseModel):
+    username: str
+    right: str
+    
+@api.post('/admin', name='Modify user right in the database')
+def post_right(user: UserRight, right: str = Depends(get_admin)):
+    users_db.loc[users_db["username"]==user.username, "right"] = user.right
+    users_db.to_csv('users_db.csv', index=False)
+    return {"Status": "User right modified in the database"} 
 
+
+# L'admin peut retirer des utilisateurs dans la BDD
+@api.delete('/admin', name='Remove user from the database')
+def remove(user: UserRight, right: str = Depends(get_admin)):
+    new_users_db = users_db[users_db["username"] != user.username]
+    new_users_db.to_csv('users_db.csv', index=False)
+    return {"Status": "User removed from the database"} 
 
 # L'utilisateur demande une prédiction sur le prix d'une ou plusieurs actions en donnant ses "Params"
 possible_trading_type = ['market', 'fundamental', 'market_and_fundamental']
@@ -99,13 +115,20 @@ class Params(BaseModel):
    trading_type: str 
 
 @api.post('/predictions', name="Get predictions according to params")
-def predict(params: Params, username: str = Depends(get_current_user)):
+def predict(params: Params, right: str = Depends(get_current_user)):
     '''
     Generate predictions according to params
     '''
     if params.trading_type not in possible_trading_type:
       raise HTTPException(status_code=400, detail='Wrong trading type')
-
+    
+    if right != "admin":
+        if params.trading_type != right:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You do not have corresponding rights",
+                headers={"WWW-Authenticate": "Basic"})
+            
     if params.time_horizon not in possible_horizon:
       raise HTTPException(status_code=400, detail='Not a valid horizon')
     
@@ -133,12 +156,18 @@ class ParamsBest(BaseModel):
    trading_type: str 
    
 @api.post('/best', name="Get best stocks in environment")
-def predict_best_stocks(params: ParamsBest, username: str = Depends(get_current_user)):
+def predict_best_stocks(params: ParamsBest, right: str = Depends(get_current_user)):
    '''
    Generate predictions on entire stocks environment according to params
    '''
    if params.trading_type not in possible_trading_type:
      raise HTTPException(status_code=400, detail='Wrong trading type')
+     
+   if right != "admin":
+       if params.trading_type != right:
+           raise HTTPException(
+               status_code=status.HTTP_401_UNAUTHORIZED,
+               detail="You do not have corresponding rights")  
 
    if params.time_horizon not in possible_horizon:
      raise HTTPException(status_code=400, detail='Not a valid horizon')
