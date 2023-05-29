3fb0768d27b4164c6590a2f4909e44a48b49d3fd
Julien <julien.chaplet@gmail.com>
2023-05-29 20:47:24 +0200

    added github workflow unit tests

---
+++ .github/workflows/unit_test.py
@@ -0,0 +1,7 @@
+import pytest
+from src.domain.utils.fetch_data import fetch_stock, fetch_fundamental
+
+def test_fetch_fundamental():
+    #fundamental = fetch_fundamental("AAPL")
+    assert all(feature in list(fundamental.columns) for feature in ['close', 'eps', 'trailing_eps_1Y', 'epsQoQ', 'surprise_%', 'expected_growth', 'previous_surprise', 'days_after_earn_report', 'Amount', 
+    'dividends_change', 'prev_div_change', 'days_after_divid_report', 'sector', 'industry'])
