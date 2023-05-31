f11e40f07da17b253b4378a49dcad568296dee54
Julien <julien.chaplet@gmail.com>
2023-05-31 10:23:34 +0200

    modified api_test for flake8

--- tests/app/api_test.py
+++ tests/app/api_test.py
@@ -20,8 +20,7 @@ def test_authentification():
     for tested_user in tested_users:
         r_ = requests.get(
             url='http://{address}:{port}/permissions'.format(address=api_address, port=api_port),
-            auth=HTTPBasicAuth(tested_user['username'], tested_user['password'])
-                        )
+            auth=HTTPBasicAuth(tested_user['username'], tested_user['password']))
         r.append(r_)
 
     for i, r_ in enumerate(r):
