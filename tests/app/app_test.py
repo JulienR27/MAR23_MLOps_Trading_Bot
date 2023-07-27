23e26fe543a2acfd694f08c9946e854a8215e829
Julien <julien.chaplet@gmail.com>
2023-07-28 00:02:06 +0200

    improved app_test.py according to pyflake

--- tests/app/app_test.py
+++ tests/app/app_test.py
@@ -5,20 +5,24 @@ import os
 
 os.environ["PATH_TO_DATABASE"] = "test_users_db.csv"
 
+
 class User(BaseModel):
     username: str
     password: str
     right: str
 
+
 class UserRight(BaseModel):
     username: str
     right: str
+
 
 def test_get_users():
     test_users_db = pd.read_csv(os.environ["PATH_TO_DATABASE"])
     test_users = get_users(test_users_db)
     assert test_users[0]["username"] == "test"
     assert test_users[0]["right"] == "tester"
+
 
 def test_create_user():
     old_test_users_db = pd.read_csv(os.environ["PATH_TO_DATABASE"])
@@ -29,8 +33,9 @@ def test_create_user():
     new_test_users_db = pd.read_csv(os.environ["PATH_TO_DATABASE"])
     nb_rows_new = new_test_users_db.shape[0]
     print(nb_rows_new)
-    assert nb_rows_new == nb_rows_old + 1 
-    assert new_test_users_db.loc[new_test_users_db["username"]=="test_create", "password"].values[0] == "test_create_password"
+    assert nb_rows_new == nb_rows_old + 1
+    assert new_test_users_db.loc[new_test_users_db["username"] == "test_create", "password"].values[0] == "test_create_password"
+
 
 def test_remove_user():
     old_test_users_db = pd.read_csv(os.environ["PATH_TO_DATABASE"])
@@ -41,5 +46,5 @@ def test_remove_user():
     new_test_users_db = pd.read_csv(os.environ["PATH_TO_DATABASE"])
     nb_rows_new = new_test_users_db.shape[0]
     print(nb_rows_new)
-    assert nb_rows_new == nb_rows_old - 1 
-    assert new_test_users_db[new_test_users_db["username"]=="test_create"].empty
+    assert nb_rows_new == nb_rows_old - 1
+    assert new_test_users_db[new_test_users_db["username"] == "test_create"].empty
